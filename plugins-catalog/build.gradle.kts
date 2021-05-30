4bfd2e8ea7716b1e041b4ec55e3f0892d6531084
Javier Segovia Córdoba <7463564+JavierSegoviaCordoba@users.noreply.github.com>
2021-05-31 00:35:26 +0200

    Bump gradle-versions-plugin to 0.39.0

--- plugins-catalog/build.gradle.kts
+++ plugins-catalog/build.gradle.kts
@@ -38,7 +38,7 @@ val mkdocs = "2.1.1"
 val nexusPublish = "1.1.0"
 val reckon = "0.13.0"
 val spotless = "5.12.5"
-val versions = "0.38.0"
+val versions = "0.39.0"
 
 // [libraries]
 "com.android.tools.build:gradle:$android"
