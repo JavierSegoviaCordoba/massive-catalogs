ba75db6f05f43962f161696b3c8fbf03fc84ed39
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-09-13 01:06:38 +0200

    Migrate `dependabot` to `renovate`

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,25 +1,10 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 enableFeaturePreview("VERSION_CATALOGS")
 
-val input = FileInputStream(file("../gradle.properties"))
-val properties = Properties().apply { load(input) }
-
-input.close()
-
-val javierscGradlePlugins: String = properties.getProperty("javierscGradlePlugins")
-
 dependencyResolutionManagement {
-    repositories {
-        mavenCentral()
-    }
+    repositories { mavenCentral() }
 
     versionCatalogs {
-        create("libs") { from(files("../libs-catalog/build/catalogs/libs.versions.toml")) }
-        create("pluginLibs") {
-            from(files("../plugins-catalog/build/catalogs/libs.versions.toml"))
-            version("javierscGradlePlugins", javierscGradlePlugins)
-        }
+        create("libs") { from(files("../gradle/libs.versions.toml")) }
+        create("pluginLibs") { from(files("../gradle/pluginLibs.versions.toml")) }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-@file:Suppress("UnstableApiUsage")
-
 rootProject.name = providers.gradleProperty("allProjects.name").forUseAtConfigurationTime().get()
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
@@ -10,6 +8,7 @@ dependencyResolutionManagement {
         mavenCentral()
         google()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
@@ -20,123 +19,3 @@ include(
 )
 
 // includeBuild("testing-catalog")
-
-/** Build local catalogs */
-listOf("libs-catalog", "plugins-catalog").onEach { path ->
-    val content = buildCatalogFile(file("$path/build.gradle.kts"))
-
-    if (content != null) {
-        file("$path/build/catalogs/").mkdirs()
-        file("$path/build/catalogs/libs.versions.toml").apply {
-            if (!exists()) createNewFile()
-            writeText(content)
-        }
-    }
-}
-
-fun buildCatalogFile(file: File): String? {
-    val content = file.readLines()
-
-    if (!content.contains("// catalog start")) return null
-
-    val catalogStartIndex = content.indexOfFirst { it.contains("// catalog start") } + 1
-    val catalogEndIndex = content.indexOfFirst { it.contains("// catalog end") } - 1
-
-    return content.subList(catalogStartIndex, catalogEndIndex).joinToString("\n") {
-        val line = it.replace("/", "")
-        when {
-            line.contains("[") -> line.replace(" ", "")
-            line.isBlank() -> line
-            line.startsWith("val ") -> VersionRef.build(line).buildAsString()
-            line.contains(":") -> Artifact.build(line).buildAsString()
-            else -> "// Line uncovered"
-        }
-    }
-}
-
-class VersionRef(private val ref: String, private val version: String) {
-
-    fun buildAsString(): String = """$ref = "$version""""
-
-    companion object {
-        fun build(line: String): VersionRef =
-            VersionRef(
-                ref = line.replace("val ", "").split(" ")[0],
-                version = line.replace(" ", "").replace("\"", "").split("=")[1]
-            )
-    }
-}
-
-class Artifact(private val group: String, private val name: String, private val version: String) {
-
-    fun buildAsString(): String = "${buildAlias()} = ${buildArtifact()}"
-
-    private val domain: String
-        get() = group.split(".")[1]
-
-    private val groupAfterDomain: String
-        get() =
-            group
-                .split(".")
-                .takeLastWhile { !it.contains(domain) }
-                .joinToString("-")
-                .cleanUnderscore()
-                .cleanHyphen()
-
-    private val cleanName: String
-        get() = name.cleanHyphen().cleanUnderscore()
-
-    private fun buildAlias(): String =
-        "$domain-${
-            ("$groupAfterDomain-$cleanName").replaceSeparators()
-        }"
-            .split("-")
-            .filter { it.isNotBlank() }
-            .joinToString("-") { it.replace("-", "") }
-            .sanitizeAlias()
-
-    private fun String.sanitizeAlias(): String {
-        return if (endsWith("plugin", true) ||
-            endsWith("plugins", true) ||
-            endsWith("bundle", true) ||
-            endsWith("bundles", true) ||
-            endsWith("version", true) ||
-            endsWith("versions", true)
-        ) {
-            this + "X"
-        } else {
-            this
-        }
-    }
-
-    private fun buildArtifact(): String =
-        if (version.isBlank()) """{ group = "$group", name = "$name" }"""
-        else """{ group = "$group", name = "$name", version.ref = "$version" }"""
-
-    private fun String.replaceSeparators(): String =
-        replace("\"", "").replace(".", "-").replace(":", "-")
-
-    private fun String.cleanHyphen(firstLowerCase: Boolean = true): String =
-        this.split('-').toStringCamelCase(firstLowerCase)
-
-    private fun String.cleanUnderscore(firstLowerCase: Boolean = true): String =
-        this.split('_').toStringCamelCase(firstLowerCase)
-
-    private fun List<String>.toStringCamelCase(firstLowerCase: Boolean = true): String =
-        joinToString("", transform = String::capitalize)
-            .mapIndexed { index: Int, char: Char ->
-                if (index == 0 && firstLowerCase) char.toLowerCase() else char
-            }
-            .joinToString("")
-
-    companion object {
-        fun build(line: String): Artifact =
-            line.replace("\"", "").replace("\$", "").split(":").run {
-                Artifact(
-                    group = get(0),
-                    name = get(1),
-                    version = getOrNull(2) ?: "",
-                )
-            }
-    }
-}
