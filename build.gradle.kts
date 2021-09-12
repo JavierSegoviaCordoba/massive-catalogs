e1bcfa3a51b20e48d0667bb523893a0b59f936a7
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-09-13 01:06:38 +0200

    Migrate `dependabot` to `renovate`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.kotlin.gradle.internal.ensureParentDirsCreated
 import scanner.ScannerTask
 import tasks.AddLibTask
 import tasks.CheckAGP
@@ -8,23 +7,10 @@ plugins {
     `javiersc-all-projects`
     `javiersc-changelog`
     `javiersc-code-analysis`
-    `javiersc-dependency-updates`
     `javiersc-docs`
     id("org.jetbrains.kotlinx.binary-compatibility-validator")
     `javiersc-nexus`
     `javiersc-readme-badges-generator`
-    `javiersc-gradle-wrapper-updater`
-    `gradle-plugins-updater`
-}
-
-// TODO: Remove when [#3286](https://github.com/dependabot/dependabot-core/issues/3286) is fixed
-allprojects {
-    repositories {
-        mavenCentral()
-        google()
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
 }
 
 tasks {
@@ -37,16 +23,4 @@ tasks {
     register<AddLibTask>("addLib")
     register<ScannerTask>("mavenScan")
     register<CheckAGP>("checkAGP")
-}
-
-subprojects.onEach { subproject ->
-    val content = buildCatalogFile(file("${subproject.projectDir}/build.gradle.kts"))
-
-    if (content != null) {
-        file("${subproject.buildDir}/catalogs/libs.versions.toml").apply {
-            ensureParentDirsCreated()
-            createNewFile()
-            writeText(content)
-        }
-    }
 }
--- buildSrc/src/main/kotlin/tasks/AddLibTask.kt
+++ buildSrc/src/main/kotlin/tasks/AddLibTask.kt
@@ -8,6 +8,7 @@ import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
 
+@Suppress("MaxLineLength")
 /**
  * ```
  * ./gradlew addLib --"projectName"="libs-catalog" --"artifactGroup"="org.example" --"artifactName"="example-name" --"artifactVersionRef"="exampleName" --"artifactVersion"="1.0.1"
